8f69a5411b0146f4bf08e6c92e5ac830148a10f2
Jaime Acosta <j.acosta@alumnos.upm.es>
2020-01-24 16:09:41 +0100

    Se cambia el static de la clase Time, se añade constructor para pasar LocalDateTime y añade return en clase MyClass

--- src/main/java/es/upm/grise/profundizacion2019/ex3/MyClass.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex3/MyClass.java
@@ -2,8 +2,13 @@ package es.upm.grise.profundizacion2019.ex3;
 
 public class MyClass {
 	
+	private Time time;
+	
+	public MyClass (Time time) {
+		this.time=time;
+	}
 	//Cambio: Añadir return String
-	public String nextTime(long seconds, Time time) {
+	public String nextTime(long seconds) {
 		String next = time.getFutureTime(seconds);
 		System.out.println(next);
 		
--- src/test/java/es/upm/grise/profundizacion2019/ex3/smokeTest.java
+++ src/test/java/es/upm/grise/profundizacion2019/ex3/smokeTest.java
@@ -6,33 +6,43 @@ import java.time.LocalDateTime;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 import static org.mockito.Mockito.*;
 
 public class smokeTest {
-	MyClass my;
+	private MyClass my;
+	private Time time;
+	
+	@Mock
+	private Time mock_time;
 	
 	@Before
 	public void setUp() {
-		my = new MyClass();
+		
+		MockitoAnnotations.initMocks(this);
+		
+		my = new MyClass(mock_time);
 	}
 
 	@Test
 	public void smoleTest() {
 		long SECONDS = 60;
-		my.nextTime(SECONDS, new Time(LocalDateTime.now()));
+		my.nextTime(SECONDS);
 	}
 	
 	@Test
 	public void Add120Seconds() {
 		long SECONDS = 120;
-		System.out.println();
-		assertEquals("2020/01/01 00:02:00", my.nextTime(SECONDS, new Time(LocalDateTime.of(2020, 01,01,00,00,00,00))));
+		time=new Time(LocalDateTime.of(2020, 01,01,00,00,00,00));
+		assertEquals("2020/01/01 00:02:00", time.getFutureTime(SECONDS));
 	}
 	
 	@Test
-	public void smokeTest() {
-		Time time=mock(Time.class);
-		
-		
+	public void verifyMethodCalls() {
+		my.nextTime((long)120);
+		verify(mock_time, times(1)).getFutureTime((long)120);
+				
 	}
 }
